6941bb2a59eaeef4d9814291b1fb58f9a632ca6a
Sahil Afrid Farookhi <msafarookhi@gmail.com>
2021-08-08 00:47:13 +0530

    WIP: display json content using js

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-function main() {
+function view() {
     fetch("play.json")
         .then(response => response.json())
         .then(data => {
             console.log(data)
+            for (let q in data.questions) {
+                document.querySelector("#test").innerHTML = data.questions[q].question + "\n"
+            }
         })
 }
