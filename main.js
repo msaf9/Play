6f35a45d17910dceb540b55767599dfdfb7f73a5
Sahil Afrid Farookhi <msafarookhi@gmail.com>
2021-08-09 23:01:37 +0530

    externalized js logic

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
 function view() {
-    fetch("play.json")
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-            for (let q in data.questions) {
-                document.querySelector("#test").innerHTML = data.questions[q].question + "\n"
-            }
+    fetch('play.json')
+        .then(function(response) {
+            return response.json();
         })
+        .then(function(data) {
+            appendData(data);
+        })
+        .catch(function(err) {
+            console.log('error: ' + err);
+        });
+
+    function appendData(data) {
+        var mainContainer = document.getElementById("myData");
+        console.log(data);
+        for (var i = 0; i < data.length; i++) {
+            var div = document.createElement("div");
+            div.innerHTML = (i + 1) + '. ' + data[i].question + ' ' + data[i].answers.A + ' ' + data[i].answers.B + ' ' + data[i].answers.C + ' ' + data[i].answers.D;
+            mainContainer.appendChild(div);
+        }
+    }
 }
